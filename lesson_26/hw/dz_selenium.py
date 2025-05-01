d86b5acf0999c9d2b583837cecd8b5df213a0158
Sergey <bikeshop.dnipro@gmail.com>
2025-05-01 14:00:15 +0300

    HW26 start commit

---
+++ lesson_26/hw/dz_selenium.py
@@ -0,0 +1,60 @@
+import time
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s][%(levelname)s] - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',
+    handlers=[logging.FileHandler('check_frames.log')],
+)
+
+logger = logging.getLogger(__name__)
+
+
+def search_and_check_frames_for_correct_input(url:str, correct_answers: list[str]):
+    driver = webdriver.Firefox()
+    driver.get(url)
+
+    # Search frames in HTML file
+    frames = driver.find_elements(By.TAG_NAME, "iframe")
+
+    for frame_index in range(len(frames)):
+        frame = frames[frame_index]
+
+        try:
+            # Get current frame instance and secret phrase to input
+            password = correct_answers[frame_index]
+
+            # Switch to frame from main window
+            driver.switch_to.frame(frame)
+
+            driver.find_element(By.TAG_NAME, "input").send_keys(password)
+            driver.find_element(By.XPATH, "//button[text()='Перевірити']").click()
+            time.sleep(1)
+
+            alert = Alert(driver)
+            alert_text = alert.text
+            alert.accept()
+
+            if alert_text == 'Верифікація пройшла успішно!':
+                logger.info(f"URL:{url}, Frame:'{frame.get_attribute("id")}' - Verification PASSED!")
+            else:
+                logger.info(f"URL:{url}, Frame:'{frame.get_attribute("id")}' - Verification FAILED!")
+
+            # Switch back from frame to main window
+            driver.switch_to.default_content()
+
+        except Exception as e:
+            logger.error(f"During processing URL:{url}, Frame:'{frame.get_attribute('id')}' - raised exception: {e}")
+
+    # Close browser
+    driver.quit()
+
+if __name__ == '__main__':
+    url = "http://localhost:8000/dz.html"
+    correct_answers = ['Frame1_Secret']
+    search_and_check_frames_for_correct_input(url=url, correct_answers=correct_answers)
